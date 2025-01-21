caef00174332b7fcb7752307c63cd8f2e4338dd3
KNamuuu <knamuuu@naver.com>
2025-01-22 02:14:26 +0900

    feat(WIP): 가챠 성공모달

--- src/app/gacha/page.tsx
+++ src/app/gacha/page.tsx
@@ -1,11 +1,43 @@
-import React from "react";
+"use client";
 
-import GachaMachineWithThree from "@/components/gacha/GachaMachineWithThree";
+import Image from "next/image";
+import React, { useState } from "react";
+
+import { GachaMachineR3F } from "@/components/gacha/GachaMachineR3F";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from "@/components/ui/dialog";
 
 export default function GachaPage() {
+  const [open, setOpen] = useState(true);
+
   return (
-    <div>
-      <GachaMachineWithThree />
+    <div className="size-full h-screen min-h-dvh">
+      <GachaMachineR3F />
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>가챠 결과</DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col items-center gap-6">
+            <div>
+              <Image
+                src="/images/sparkler.png"
+                alt="sparkler"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="text-center">
+              <div className="mb-4 text-2xl font-semibold">
+                <div>축하합니다 !</div>
+                <div>상품에 당첨되었습니다</div>
+              </div>
+              <div>상품 수령을 위해 로그인을 진행해 주세요</div>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
+
     </div>
   );
 }
